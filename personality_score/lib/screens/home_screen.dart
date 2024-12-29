84148f2c3f3832603d736779e85d30a4cbeeccd3
hm-shaug <shaug@hm.edu>
2024-12-29 18:45:10 +0100

    changed path

--- personality_score/lib/screens/home_screen.dart
+++ personality_score/lib/screens/home_screen.dart
@@ -194,6 +194,11 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Column(
         children: [
           SizedBox(height: 200),
+          Image.asset(
+              'assets/ps_background_ai.jpg',
+              fit: BoxFit.cover,
+              width: screenWidth,
+            ),
           _buildHeaderSection(context, screenHeight, screenWidth),
           SizedBox(height: 200),
           _buildVideoSection1(),
